a397e474523097e18bd8379b8ef5f75d03a337b0
mburnett <mike.burnett@disneystreaming.com>
2019-10-03 17:49:26 -0400

    removing ts comment

--- src/Person.js
+++ src/Person.js
@@ -2,14 +2,6 @@ import React from 'react';
 import classNames from 'classnames';
 import styles from './Person.css'; // eslint-disable-line no-unused-vars
 
-/*
-export interface IFamilyExtNode extends IFamilyNode {
-  top: number;
-  left: number;
-  hasSubTree: boolean;
-}
-*/
-
 export default function Person({ handleClick, isRoot, node, style }) {
     const rootStyles = isRoot ? ['inner', node.gender, 'isRoot'] : ['inner', node.gender];
     return (
