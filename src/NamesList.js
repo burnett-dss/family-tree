e1cf0c6c6437a138c9f60a9a932c00bc9dec15cc
Joe Tessler <jrt@google.com>
2019-11-17 11:22:10 -0500

    Add missing key attribute to NamesList

--- src/NamesList.js
+++ src/NamesList.js
@@ -5,7 +5,7 @@ class NamesList extends React.Component {
     let namesLiList = [];
     for (let i in this.props.namesList) {
       let nameString = this.props.namesList[i];
-      namesLiList.push(<li>{nameString}</li>);
+      namesLiList.push(<li key={i}>{nameString}</li>);
     }
 
     return(
